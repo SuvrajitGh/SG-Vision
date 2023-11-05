55306b1959d38e9aeb4d6728c2033cfcdb8d45eb
SuvrajitGhosh <ghoshsuvrajit17@gmail.com>
2023-11-06 00:22:53 +0530

    changed the color of the project

--- app/components/projects/page.tsx
+++ app/components/projects/page.tsx
@@ -5,7 +5,7 @@ const Project = () => {
   return (
     <>
       <section className="grid gap-8 p-5 mt-5 md:p-5 bg-[#000]">
-      <h1 className="text-4xl tracking-[15px] text-center uppercase text-indigo-600">Projects</h1>
+      <h1 className="text-4xl tracking-[15px] text-center uppercase">Projects</h1>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl::grid-cols-4 gap-5">
      
           {projectdata.map((project) => (
